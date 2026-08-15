e41560e062f7ddd34990792f613d6ae45c1e1aa4
Simranpathak87 <pathaksimran087@gmail.com>
2026-08-15 22:29:31 +0530

    Create ChatMessage.java

---
+++ src/main/java/com/chatapp/ChatMessage.java
@@ -0,0 +1,57 @@
+package com.chatapp.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ChatMessage {
+	
+	
+	
+		    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private String sender;
+	    private String receiver;
+	    private String message;
+	    private LocalDateTime time;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getSender() {
+			return sender;
+		}
+		public void setSender(String sender) {
+			this.sender = sender;
+		}
+		public String getReceiver() {
+			return receiver;
+		}
+		public void setReceiver(String receiver) {
+			this.receiver = receiver;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public LocalDateTime getTime() {
+			return time;
+		}
+		public void setTime(LocalDateTime time) {
+			this.time = time;
+		}
+		
+	   
+	}
+
+
